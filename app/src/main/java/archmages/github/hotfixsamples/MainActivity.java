35038323b26d127e3c5fc47be0a918fc2955136c
jason0539 <jason0539@163.com>
2016-09-03 09:08:57 +0800

    modify doc

--- app/src/main/java/archmages/github/hotfixsamples/MainActivity.java
+++ app/src/main/java/archmages/github/hotfixsamples/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView textView = (TextView) findViewById(R.id.textView);
-        //修改类
+        //修改类,如果不修改,那么patch包中将没有该类
         BugClass bugClass = new BugClass();
         textView.setText(bugClass.bug());
         //新增类
